ffc47802679f70bcfb21aee945d349945eb5db65
Diogo Castro <dc@diogocastro.com>
2018-10-23 22:20:10 +0100

    Moved examples to their own module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Flatbuffers
+import Examples
 
 main :: IO ()
 main = go
--- src/FlatBuffers.hs
+++ src/FlatBuffers.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
 
-module Flatbuffers where
+module FlatBuffers where
 
 import           Control.Monad.State
 import qualified Data.ByteString           as BS
@@ -19,84 +18,6 @@ import qualified Data.Text.Lazy            as TL
 import qualified Data.Text.Lazy.Encoding   as TL
 import           Data.Word
 import           Debug.Trace
-
-go =
-  putStrLn "Dumping" >>
-  BSL.writeFile "bs.txt" (toLazyByteString nested)
-
-st =
-  root [
-    scalar struct [
-      padded 4 $ int32 9876,
-      struct [
-        int64 765,
-        int64 98760
-      ]
-    ]
-  ]
-
-vectors =
-  root [
-    vector [text "bye 👬", string "bye 👬", byteString "bye 👬", lazyByteString "bye 👬", lazyText "bye 👬"],
-    vector [scalar int32 12, scalar int32 34],
-    vector [scalar int64 23, scalar int64 45]
-  ]
-
-variety =
-  root [
-    scalar word8 8,
-    scalar word8 2,
-    table [
-      scalar int32 670
-    ],
-    scalar bool True
-  ]
-
-obj =
-  root [
-    scalar int32 1,
-    text "hello",
-    scalar int64 978,
-    text "hello",
-    missing
-  ]
-
-nested = 
-  root [
-    scalar int32 99,
-    table [
-      scalar int32 111,
-      string "hello"
-    ],
-    table [
-      scalar int32 111,
-      string "hello"
-    ]
-  ]
-
-lowLevelSimple2 =
-  runRoot $ do
-    hello <- string' "hello"
-    root'
-      [ int32 1
-      , hello
-      , int64 978
-      , hello
-      , missing'
-      ]
-
-lowLevelNested =
-  runRoot $ do
-    hello <- string' "hello"
-    obj <- table'
-      [ int32 111
-      , hello
-      ]
-    root'
-      [ int32 99
-      , obj
-      , obj
-      ]
 
 
 type BytesWritten = Int
