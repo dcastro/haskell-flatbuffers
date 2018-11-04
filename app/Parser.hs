b871276d077095c7f24683f1617dee34551f6b05
Diogo Castro <dc@diogocastro.com>
2018-11-04 22:01:02 +0000

    Added parser for vector declarations, and other primitive types

--- app/Parser.hs
+++ app/Parser.hs
@@ -33,11 +33,23 @@ data Field = Field { fieldIdent :: Ident, fieldType :: Type }
   deriving Show
 
 data Type
-  = Tbool
-  | Tbyte
-  | Tubyte
-  | Tshort
-  | Tushort
+  -- numeric
+  =  Tint8
+  | Tint16
+  | Tint32
+  | Tint64
+  | Tword8
+  | Tword16
+  | Tword32
+  | Tword64
+  -- floating point
+  | Tfloat
+  | Tdouble
+  -- others
+  | Tbool
+  | Tstring
+  | Vector Type
+  | Tident Ident
   deriving (Show)
 
 sc :: Parser ()
@@ -52,12 +64,14 @@ lexeme = L.lexeme sc
 symbol :: String -> Parser String
 symbol = L.symbol sc
 
+rword :: String -> Parser ()
+rword w = (lexeme . try) (string w *> notFollowedBy alphaNumChar)
+
 curly :: Parser a -> Parser a
 curly = between (symbol "{") (symbol "}")
 
 semi :: Parser String
 semi = symbol ";"
-
 
 ident :: Parser Ident
 ident = (lexeme . try) identifier
@@ -66,18 +80,31 @@ ident = (lexeme . try) identifier
 
 typ :: Parser Type
 typ =
-  symbol "bool" $> Tbool <|>
-  symbol "byte" $> Tbyte <|>
-  symbol "ubyte" $> Tubyte <|>
-  symbol "short" $> Tshort <|>
-  symbol "ushort" $> Tushort
+  Tint8 <$ (symbol "int8" <|> symbol "byte") <|>
+  Tint16 <$ (symbol "int16" <|> symbol "short") <|>
+  Tint32 <$ (symbol "int32" <|> symbol "int") <|>
+  Tint64 <$ (symbol "int64" <|> symbol "long") <|>
+  Tword8 <$ (symbol "uint8" <|> symbol "ubyte") <|>
+  Tword16 <$ (symbol "uint16" <|> symbol "ushort") <|>
+  Tword32 <$ (symbol "uint32" <|> symbol "uint") <|>
+  Tword64 <$ (symbol "uint64" <|> symbol "ulong") <|>
+
+  Tfloat <$ (symbol "float32" <|> symbol "float") <|>
+  Tdouble <$ (symbol "float64" <|> symbol "double") <|>
+
+  Tbool <$ symbol "bool" <|>
+  Tstring <$ symbol "string" <|>
+  Vector <$> vector typ <|>
+  Tident <$> ident
+  where
+    vector = between (symbol "[" *> (notFollowedBy (symbol "[") <|> fail "nested vector types not supported" )) (symbol "]")
 
 field :: Parser Field
 field = (Field <$> ident <*> (symbol ":" >> typ)) <* semi
 
 typeDecl :: Parser TypeDecl
 typeDecl = do
-  tt <- symbol "table" $> Table <|> symbol "struct" $> Struct
+  tt <- rword "table" $> Table <|> rword "struct" $> Struct
   i <- ident
   fs <- curly (NE.some field)
   pure $ TypeDecl tt i fs
