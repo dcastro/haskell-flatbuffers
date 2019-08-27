6f89329876b119ee7eda7ab40c8ac22e253af4a8
Diogo Castro <dc@diogocastro.com>
2019-08-27 23:50:56 +0100

    Upgraded hspec-expectations-pretty-diff, fixes call stacks in test failures

--- test/TestImports.hs
+++ test/TestImports.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE ImplicitParams #-}
-{-# LANGUAGE ConstraintKinds #-}
-
 module TestImports
   ( module H
   , HasCallStack
@@ -22,15 +19,13 @@ import           Data.Aeson.Encode.Pretty       ( encodePretty )
 import qualified Data.ByteString.Lazy.UTF8      as BSLU
 import qualified Data.Text.Lazy                 as TL
 
-import           GHC.Stack                      ( CallStack )
+import           GHC.Stack                      ( HasCallStack )
 
 import           Test.HUnit                     ( assertFailure )
 import           Test.Hspec.Core.Hooks          as H
 import           Test.Hspec.Core.Spec           as H
 import           Test.Hspec.Expectations.Pretty as H
 import           Test.Hspec.Runner              as H
-
-type HasCallStack = ?loc :: CallStack
 
 -- | Useful when there's no `Show`/`Eq` instances for @a@.
 shouldBeLeft :: HasCallStack => Show e => Eq e => Either e a -> e -> Expectation
@@ -64,8 +59,6 @@ evalRightJust = evalRight >=> evalJust
 -- | Like `expectationFailure`, but returns @IO a@ instead of @IO ()@.
 expectationFailure' :: HasCallStack => String -> IO a
 expectationFailure' = Test.HUnit.assertFailure
-  where
-    ?callStack = ?loc
 
 -- | Allows Json documents to be compared (using e.g. `shouldBe`) and pretty-printed in case the comparison fails.
 newtype PrettyJson = PrettyJson J.Value
