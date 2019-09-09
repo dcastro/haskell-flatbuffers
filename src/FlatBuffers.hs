f70b930df0d52f31781ecd12b09460b7034c209a
Diogo Castro <dc@diogocastro.com>
2019-09-09 13:09:22 +0100

    Exposed `TH.Options`, `IsStruct`, `Alignment` and `InlineSize`

--- src/FlatBuffers.hs
+++ src/FlatBuffers.hs
@@ -7,27 +7,33 @@ module FlatBuffers
     -- * Creating a flatbuffer
   , W.encode
   , W.encodeWithFileIdentifier
-  , W.WriteStruct
-  , W.WriteTable
-  , W.WriteUnion
   , W.none
 
     -- * Reading a flatbuffer
   , R.decode
   , R.checkFileIdentifier
-  , R.Struct
-  , R.Table
-  , R.Union(..)
-  , R.ReadError(..)
 
   -- * File Identifier
   , FI.FileIdentifier
   , FI.HasFileIdentifier(..)
+
+  -- * Types
+  , W.WriteStruct
+  , W.WriteTable
+  , W.WriteUnion
+  , R.Struct
+  , R.Table
+  , R.Union(..)
+  , T.InlineSize(..)
+  , T.Alignment(..)
+  , T.IsStruct(..)
+  , TH.Options(..)
+  , R.ReadError(..)
   ) where
 
 import           FlatBuffers.Internal.Compiler.TH    as TH
 import           FlatBuffers.Internal.FileIdentifier as FI
 import           FlatBuffers.Internal.Read           as R
+import           FlatBuffers.Internal.Types          as T
 import           FlatBuffers.Internal.Write          as W
-
 
--- src/FlatBuffers/Internal/Types.hs
+++ src/FlatBuffers/Internal/Types.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
+{-# OPTIONS_HADDOCK not-home #-}
+
 module FlatBuffers.Internal.Types where
 
 import Data.Word
@@ -14,8 +16,8 @@ newtype InlineSize = InlineSize { unInlineSize :: Word16 }
   deriving newtype (Show, Eq, Num, Enum, Ord, Real, Integral, Bounded)
 
 -- | The memory alignment for a piece of data in a flatbuffer.
--- E.g., int32 are always aligned to 4 bytes.
--- This number should always be in the range [1, 16].
+-- E.g., `Int32` are always aligned to 4 bytes.
+-- This number should always be a power of 2 in the range [1, 16].
 newtype Alignment = Alignment { unAlignment :: Word8 }
   deriving newtype (Show, Eq, Num, Enum, Ord, Real, Integral, Bounded)
 
