7231a88180c32d42b4ceee09b3446ee4eccac542
Diogo Castro <dc@diogocastro.com>
2024-08-19 23:20:51 +0100

    `writeMany` vs `writeMany2`, no noticeable difference
    benchmarking People/Write1
    time                 330.5 ms   (230.5 ms .. 469.6 ms)
                         0.950 R²   (0.931 R² .. 1.000 R²)
    mean                 299.4 ms   (271.1 ms .. 327.7 ms)
    std dev              37.67 ms   (29.88 ms .. 39.62 ms)
    variance introduced by outliers: 37% (moderately inflated)
    
    benchmarking People/Write2
    time                 75.35 ms   (57.29 ms .. 88.14 ms)
                         0.948 R²   (0.909 R² .. 0.982 R²)
    mean                 58.74 ms   (54.50 ms .. 65.33 ms)
    std dev              9.523 ms   (6.602 ms .. 12.25 ms)
    variance introduced by outliers: 57% (severely inflated)
    
    benchmarking People/Write3
    time                 29.16 ms   (22.64 ms .. 33.52 ms)
                         0.924 R²   (0.891 R² .. 0.995 R²)
    mean                 24.27 ms   (23.05 ms .. 26.40 ms)
    std dev              3.431 ms   (2.023 ms .. 4.569 ms)
    variance introduced by outliers: 62% (severely inflated)
    
    benchmarking People/Write3WriteMany2
    time                 22.96 ms   (20.95 ms .. 25.62 ms)
                         0.973 R²   (0.960 R² .. 0.995 R²)
    mean                 25.71 ms   (23.78 ms .. 29.05 ms)
    std dev              5.601 ms   (2.990 ms .. 7.887 ms)
    variance introduced by outliers: 78% (severely inflated)
    
    benchmarking People/Write3Copy
    time                 19.68 ms   (17.54 ms .. 20.90 ms)
                         0.959 R²   (0.910 R² .. 0.987 R²)
    mean                 27.86 ms   (25.60 ms .. 30.29 ms)
    std dev              5.569 ms   (4.547 ms .. 7.413 ms)
    variance introduced by outliers: 78% (severely inflated)
    
    benchmarking People/Write3Public
    time                 24.27 ms   (19.86 ms .. 29.56 ms)
                         0.882 R²   (0.774 R² .. 0.946 R²)
    mean                 28.94 ms   (26.71 ms .. 30.95 ms)
    std dev              4.625 ms   (4.102 ms .. 5.064 ms)
    variance introduced by outliers: 65% (severely inflated)
    
    benchmarking People/Write3PublicWriteMany2
    time                 30.58 ms   (26.33 ms .. 36.17 ms)
                         0.919 R²   (0.860 R² .. 0.971 R²)
    mean                 27.49 ms   (25.71 ms .. 29.43 ms)
    std dev              4.275 ms   (3.227 ms .. 4.848 ms)
    variance introduced by outliers: 65% (severely inflated)

--- bench/Compare.hs
+++ bench/Compare.hs
@@ -92,6 +92,26 @@ write3 people =
 
     W3.writeTable 1 $ W3.writeOffsetTableField 0 peopleVector
 
+-- Like `write3`, but uses the overloaded `writeMany2`
+write3WriteMany2 :: V.Vector Person -> BS.ByteString
+write3WriteMany2 people =
+  W3.encode W3.defaultWriteSettings do
+    peopleTables <- W3.writeMany2 people \person -> do
+      name <- W3.writeText person.personName
+      friends <- W3.toVector =<< W3.writeMany2 person.personFriends W3.writeText
+      W3.writeTable @Person 3 $ mconcat
+        [
+          W3.writeInt32TableField 0 person.personAge
+          ,
+          W3.writeOffsetTableField 1 name
+          ,
+          W3.writeOffsetTableField 2 friends
+        ]
+    peopleVector <- W3.toVector peopleTables
+
+    W3.writeTable 1 $ W3.writeOffsetTableField 0 peopleVector
+
+
 write3Copy :: V.Vector Person -> BS.ByteString
 write3Copy people =
   W3C.encode W3C.defaultWriteSettings do
@@ -119,6 +139,15 @@ write3Public people =
       W3P.person (Just person.personAge) (Just name) (Just friends)
     W3P.people (Just peopleVector)
 
+write3PublicWriteMany2 :: V.Vector Person -> BS.ByteString
+write3PublicWriteMany2 people =
+  W3.encode W3.defaultWriteSettings do
+    peopleVector <- W3.toVector =<< W3.writeMany2 people \person -> do
+      name <- W3.writeText person.personName
+      friends <- W3.toVector =<< W3.writeMany2 person.personFriends W3.writeText
+      W3P.person (Just person.personAge) (Just name) (Just friends)
+    W3P.people (Just peopleVector)
+
 groups :: [Benchmark]
 groups =
   [ bgroup "People"
@@ -129,9 +158,13 @@ groups =
     ,
       bench "Write3" $ nf write3 $ mkPeople peopleCount friendsCount
     ,
+      bench "Write3WriteMany2" $ nf write3WriteMany2 $ mkPeople peopleCount friendsCount
+    ,
       bench "Write3Copy" $ nf write3Copy $ mkPeople peopleCount friendsCount
     ,
       bench "Write3Public" $ nf write3Public $ mkPeople peopleCount friendsCount
+    ,
+      bench "Write3PublicWriteMany2" $ nf write3PublicWriteMany2 $ mkPeople peopleCount friendsCount
     ]
   , bgroup "Unions"
     [
